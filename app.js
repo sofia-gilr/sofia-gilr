444cb5861358410a40c8f13d3ce501cd6770580c
sofia-gilr <123768199+sofia-gilr@users.noreply.github.com>
2023-03-10 13:55:11 -0500

    lecture 4

--- app.js
+++ app.js
@@ -25,14 +25,23 @@ document.addEventListener("DOMContentLoaded", () => {
   //----------------- lecture_03 ----------------------------------//
   function crearTablero() {
     for (let i = 0; i < cardAdj.length; i++) {
-
-      var carta = document.createElement("img"); 
-      carta.setAttribute("src", "images/reverso.png"); 
-      carta.setAttribute("data-id", i); 
-      carta.addEventListener("click", voltearCarta); 
-      cuadricula.appendChild(carta); 
-      
-}
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "images/reverso.png");
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("click", voltearCarta);
+      cuadricula.appendChild(carta);
+    }
   }
-}
-);
+  //----------------- lecture_04 ----------------------------------//
+
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1000);
+    }
+  }
+  crearTablero();
+});
